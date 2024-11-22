9eaf3993b7de7c44ccf0d17aa0156b164bfe0018
Maxmerock123 <Maxmerock@gmail.com>
2024-11-22 13:58:58 +0700

    feat: webSocket Server

---
+++ cmd/chat-room/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+// Websocket Upgrader
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // allow all origins for simplicity
+	},
+}
+
+func wsHandler(c *gin.Context) {
+	// Upgrade HTTP connection to websocket
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Failed to upgrade to websocket: ", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("client connected")
+
+	for {
+		// Read message from client
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		log.Printf("Received: %s\n", message)
+
+		// Echo message back to client
+		err = conn.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	// Initialize Gin router
+	router := gin.Default()
+
+	// Define a webSocket
+	router.GET("/ws", wsHandler)
+
+	// Start the server
+	port := ":8080"
+	log.Println("Sever running on port", port)
+	if err := router.Run(port); err != nil {
+		log.Fatal("Server failed:", err)
+	}
+}
